0ca86fc431713344242e2ec35b4060fb5492e8e6
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-04 21:40:37 +0200

    [fix]: remove trim() from solution

--- 8kyo/javascript/remove-string-spaces.js
+++ 8kyo/javascript/remove-string-spaces.js
@@ -1,6 +1,6 @@
 //Write a function that removes the spaces from the string, then return the resultant string.
 function noSpace(x){
-    return x.trim().split(' ').join('')
+    return x.split(' ').join('')
 }
 
 
