e314b5ee6a664069fe27b59d89fcfe3034296a7d
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-05 20:31:34 +0200

    [Javascript]-[8kyu]: Complete double numbers in array kata

---
+++ 8kyo/javascript/double-array.js
@@ -0,0 +1,16 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+ */
+
+function doubleArray(arr) {
+    return arr.map(num => num *2)
+}
+
+
+console.log(doubleArray([1,2,3,4]));
