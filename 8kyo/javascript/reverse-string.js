e6ce69a9f3ecaa24e09bb621f7c15bb68abf1051
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-05 02:06:25 +0200

    [Javascript]-[8kyu]: Complete string reverse kata

---
+++ 8kyo/javascript/reverse-string.js
@@ -0,0 +1,9 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+ */
+
+function reverseString(string) {
+    return string.split('').reverse().join('');
+}
+
+console.log(reverseString('Helow'));
