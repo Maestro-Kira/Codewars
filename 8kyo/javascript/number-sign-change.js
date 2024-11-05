9fa990a24756f1c434d3b4d3cc9889bc9eb0dc46
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-05 02:17:04 +0200

    [Javascript]-[8kyu]: Complete change number sign kata

---
+++ 8kyo/javascript/number-sign-change.js
@@ -0,0 +1,9 @@
+/*
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+ */
+
+function changeSign(num){
+    return -num
+}
+console.log(changeSign(-4))
+console.log(changeSign(5))
