3946978236135b283265cd4ac882ef466c223ef2
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-06 20:09:07 +0200

    [Javascript]-[8kyu]: Complete Reduce But Grow kata

--- katas/javascript/js/main.js
+++ katas/javascript/js/main.js
@@ -1,19 +1,13 @@
 /*
-Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
-
-Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
-
-Note: The function accepts an integer and returns an integer.
-
-Happy Coding!
-
-
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
  */
 
-function squareDigits(num){
-    return +num.toString().split('').map(number => Math.pow(parseInt(number), 2)).join('')
+
+function resolve(arr){
+    return arr.reduce((acc, cur) => acc * cur)
 }
 
-console.log(squareDigits(12345));
+
+console.log(resolve([1, 2, 3, 4]))
