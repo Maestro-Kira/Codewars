16a4a10af5df8166c2f681e4499d9630378839f1
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-06 03:34:29 +0200

    [Javascript]-[8kyu]: Complete basic mathematical operations kata.

--- katas/javascript/js/main.js
+++ katas/javascript/js/main.js
@@ -1,15 +1,21 @@
 /*
-Write a function that counts all vowels in a string
+Your task is to create a function that does four basic mathematical operations.
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
  */
-function countVowels(str) {
-    return str.split('')
-        .filter((letter) =>
-            (letter === 'a') ||
-            (letter === 'e') ||
-            (letter === 'i') ||
-            (letter === 'o') ||
-            (letter === 'u'))
-        .length
+function calculate(operator, num1, num2) {
+    return (operator === '+') ? num1 + num2 :
+           (operator === '*') ? num1 * num2 :
+           (operator === '/') ? num1 / num2 :
+           (operator === '-') ? num1 - num2 :
+           null
 }
-
-console.log(countVowels('abracadabra'))
+console.log(calculate('-', 2,8))
+console.log(calculate('+', 2,8))
+console.log(calculate('*', 2,8))
+console.log(calculate('/', 8,2))
