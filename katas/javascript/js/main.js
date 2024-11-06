f181822c6cb061d7e378c72347359d297fe02895
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-06 19:20:16 +0200

    [Javascript]-[7kyu]: Complete Square Number kata

--- katas/javascript/js/main.js
+++ katas/javascript/js/main.js
@@ -1,17 +1,19 @@
 /*
-Trolls are attacking your comment section!
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
 
-Your task is to write a function that takes a string and return a new string with all vowels removed.
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
 
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: The function accepts an integer and returns an integer.
 
-Note: for this kata y isn't considered a vowel.
+Happy Coding!
+
+
  */
 
-function noVowels(str) {
-    return str.replace(/[aeiuo]/gi, '')
+function squareDigits(num){
+    return +num.toString().split('').map(number => Math.pow(parseInt(number), 2)).join('')
 }
 
-console.log(noVowels("This website is for losers LOL!"))
+console.log(squareDigits(12345));
