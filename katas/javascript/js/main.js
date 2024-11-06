dbcdbc2563d6e1aca3289f38b564ab19fa48536c
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-06 18:46:49 +0200

    [Javascript]-[7kyu]: Complete No Vowels kata

--- katas/javascript/js/main.js
+++ katas/javascript/js/main.js
@@ -1,32 +1,17 @@
 /*
-You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+Trolls are attacking your comment section!
 
-Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
 
-[]                                -->  "no one likes this"
-["Peter"]                         -->  "Peter likes this"
-["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
  */
 
-
-function likes(arr) {
-    if (arr.length === 0) {
-        return 'no one likes this';
-    } else if (arr.length === 1) {
-        return `${arr[0]} likes this`
-    } else if (arr.length === 2) {
-        return `${arr[0]}, ${arr[1]} likes this`
-    } else if (arr.length === 3) {
-        return `${arr[0]}, ${arr[1]}, ${arr[2]} likes this`
-    } else {
-        return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
-    }
+function noVowels(str) {
+    return str.replace(/[aeiuo]/gi, '')
 }
 
-
-console.log(likes([]))
-console.log(likes(['Peter']))
-console.log(likes(['Peter', "Alex"]))
-console.log(likes(["Alex", "Jacob", "Mark", "Max"]))
+console.log(noVowels("This website is for losers LOL!"))
