7c56d0dbcf715ed8e967399cc4f7524777f23bfc
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-11-18 21:46:31 +0200

    [Javascript]-[8kyu]: Complete String to Array kata

--- katas/javascript/js/codewars-solutions.js
+++ katas/javascript/js/codewars-solutions.js
@@ -1,14 +1,14 @@
 /*
-Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+Write a function to split a string and convert it into an array of words.
 
-Note: input will never be an empty string
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
  */
 
-function fakeBin(x){
-    return x
-        .split('')
-        .map(n => n < 5 ? '0' : '1')
-        .join('')
+function stringToArray(string){
+    return string.split(' ');
 }
 
-console.log(fakeBin('45385593107843568')); //01011110001100111
+console.log(stringToArray('hello i am bob'));
