f0f0d5be5f36008cee87d6460d17a88c80881c2a
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-12-11 00:00:12 +0200

    [Javascript]-[8kyu]: Complete shortest word kata

---
+++ katas/javascript/js/shortest-word.js
@@ -0,0 +1,13 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+ */
+
+function shortestWord(str) {
+     let wordLengthArray = str.split(' ').map(word => word.length);
+     return Math.min(...wordLengthArray)
+}
+
+console.log(shortestWord("bitcoin take over the world maybe who knows perhaps"))
