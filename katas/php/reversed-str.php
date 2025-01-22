392668d8bda4c31430c79caedfa6d953bfa06501
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-01-22 20:19:06 +0200

    [PHP]-[8kyu]: Complete Reversed Strings kata

---
+++ katas/php/reversed-str.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+  
+function solution(string $str): string {
+    
+};
